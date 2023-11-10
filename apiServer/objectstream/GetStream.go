49ae5759599830139b66172278cde1f6e39f638c
soraQaQ <1093164900@qq.com>
2023-11-10 14:17:30 +0800

    getStrem

---
+++ apiServer/objectstream/GetStream.go
@@ -0,0 +1,41 @@
+/*
+ * @Author: Aquamarine
+ * @Date: 2023-11-04 21:16:39
+ * @LastEditTime: 2023-11-04 22:02:00
+ * @LastEditors: your name
+ * @Description:
+ * @FilePath: /Distributed/apiServer/objectstream/GetStream.go
+ */
+package objectstream
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type GetStream struct {
+	reader io.Reader
+}
+
+func newGetStream(url string) (*GetStream, error) {
+	r, e := http.Get(url)
+	if e != nil {
+		return nil, e
+	}
+	if r.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("dataServer return hhtp code %d", r.StatusCode)
+	}
+	return &GetStream{r.Body}, nil
+}
+
+func NewGetStream(server, object string) (*GetStream, error) {
+	if server == "" || object == "" {
+		return nil, fmt.Errorf("invalid server %s object %s", server, object)
+	}
+	return newGetStream("http://" + server + "/objects/" + object)
+}
+
+func (r *GetStream) Read(p []byte) (n int, err error) {
+	return r.reader.Read(p)
+}
